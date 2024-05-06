41d05fd533085858ff15acf00e27beff1b88424b
salvijus2008 <169093068+salvijus2008@users.noreply.github.com>
2024-05-06 22:13:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m, n, k;
+    cout << "Iveskite mokiniu skaiciu " << endl;
+    cin>> n ;
+    cout<<"Iveskite knygos lapu skaiciu "<<endl;
+    cin>> m ;
+
+    k=n*m;
+
+    cout<<"Tau reiks tiek popieriaus lapu: " << k <<endl;
+
+
+
+
+
+    return 0;
+}
