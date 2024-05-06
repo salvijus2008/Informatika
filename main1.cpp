ebfc8560fa70a5268908d5688efb3ce172ead036
salvijus2008 <169093068+salvijus2008@users.noreply.github.com>
2024-05-06 22:36:05 +0300

    Add files via upload

--- main1.cpp
+++ main1.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
+
+
+const double KELVIN_OFFSET = 273.16;
+const double REOMUR_FACTOR = 0.8;
+
+int main() {
+    double kelvinai;
+    cout << "iveskite temperatura kelvino skaleje: ";
+    cin >> kelvinai;
+
+
+    double celsijai = kelvinai - KELVIN_OFFSET;
+
+    double farenheitai = celsijai * 9 / 5 + 32;
+
+
+    double reomurai = celsijai * REOMUR_FACTOR;
+
+    cout << fixed << setprecision(2);
+    cout << celsijai << " Celsijaus laipsniu" << endl;
+    cout << farenheitai << " Farenheito laipsniu" << endl;
+    cout << reomurai << " Reomiuro laipsniu" << endl;
+
+    return 0;
+}
