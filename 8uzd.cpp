8c061c8004b1a2682ac25d709ee84ca0b68ca576
salvijus2008 <169093068+salvijus2008@users.noreply.github.com>
2024-05-12 22:40:15 +0300

    Update 8uzd.cpp

--- 8uzd.cpp
+++ 8uzd.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Iveskite trizenkli skaiciu: ";
+    cin >> n;
+
+    int skaicius = n;
+    int suma = 0;
+
+    while (skaicius != 0) {
+        int skaitmuo = skaicius % 10;
+        suma += skaitmuo * skaitmuo * skaitmuo;
+        skaicius /= 10;
+    }
+
+    if (suma == n)
+        cout << n << " yra Armstrongo skaicius." << endl;
+    else
+        cout << n << " nera Armstrongo skaicius." << endl;
+
+    return 0;
+}
