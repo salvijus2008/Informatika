651d13ba427bce16942986d64cd790c5235c9e87
salvijus2008 <169093068+salvijus2008@users.noreply.github.com>
2024-05-12 22:46:15 +0300

    Update 9uzd.cpp

--- 9uzd.cpp
+++ 9uzd.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Ivesk tris skaicius: ";
+    cin >> a >> b >> c;
+
+    int didziausias = max(a, max(b, c));
+    int maziausias = min(a, min(b, c));
+
+    cout << "Didziausias skaicius: " << didziausias << endl;
+    cout << "Maziausias skaicius: " << maziausias << endl;
+
+    return 0;
+}
 
