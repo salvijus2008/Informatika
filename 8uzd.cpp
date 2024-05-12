0bf3608a0337238c4445290711d4634a6766993c
salvijus2008 <169093068+salvijus2008@users.noreply.github.com>
2024-05-12 22:53:32 +0300

    Update 8uzd.cpp

--- 8uzd.cpp
+++ 8uzd.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cout << "Ivesk staciakampio krastiniu ilgius: ";
+    cin >> a >> b >> c >> d;
+
+    if ((a == c && b + d <= a) || (b == d && a + c <= b))
+        cout << "Kurmiui zemes sklypo ribas pazymeti pavyks" << endl;
+    else
+        cout << "Kurmiui zemes sklypo ribu pazymeti nepavyks" << endl;
+
+    return 0;
+}
 
