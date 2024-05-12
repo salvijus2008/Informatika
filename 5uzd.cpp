93be9cffc0a6042a8e647e24b1a29bb49cea3b37
salvijus2008 <169093068+salvijus2008@users.noreply.github.com>
2024-05-12 22:57:11 +0300

    Update 5uzd.cpp

--- 5uzd.cpp
+++ 5uzd.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int hours, minutes, seconds;
+
+
+    cout << "Iveskite valandas: ";
+    cin >> hours;
+    cout << "Iveskite minutes: ";
+    cin >> minutes;
+    cout << "Iveskite sekundes: ";
+    cin >> seconds;
+
+
+    int totalSeconds = hours * 3600 + minutes * 60 + seconds;
+
+
+    totalSeconds--;
+
+
+    hours = totalSeconds / 3600;
+    totalSeconds %= 3600;
+    minutes = totalSeconds / 60;
+    seconds = totalSeconds % 60;
+
+
+    cout << "Laikrodis rode " << hours << " val " << minutes << " min " << seconds << " sek pries sekunde." << endl;
+
+    return 0;
+}
 
