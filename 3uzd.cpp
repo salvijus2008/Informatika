a56175fb52ee939b5c0270bf578b4b4770b557d2
salvijus2008 <169093068+salvijus2008@users.noreply.github.com>
2024-05-12 22:33:26 +0300

    Update 3uzd.cpp

--- 3uzd.cpp
+++ 3uzd.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int x, y, n;
+    cout<< "zuvytes kaina: ";
+    cin>>x;
+    cout<< "sraiges kaina: ";
+    cin>>y;
+    cout<<"kiek pirksi sraigiu: ";
+    cin>>n;
+    cout<<"reiks sumoketi: ";
+    cout<<n*y+n*2*x+x*2+y;
+
+    return 0;
+}
