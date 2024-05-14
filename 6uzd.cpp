a7291718342ec18318678ac030dd71ea12e9b51c
salvijus2008 <169093068+salvijus2008@users.noreply.github.com>
2024-05-14 07:25:09 +0300

    Update 6uzd.cpp

--- 6uzd.cpp
+++ 6uzd.cpp
@@ -1,1 +1,37 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int s, sh, p;
+ cout<<"svarai: ";
+ cin>>s;
+ cout<<"silingai: ";
+ cin>>sh;
+ cout<<"pencai: ";
+ cin>>p;
+ if(p == 0)
+ {
+     sh=sh-1;
+     p=11;
+     if(sh==-1)
+     {
+         s=s-1;
+         sh=19;
+         p=11;
+     }
+ }
+ else
+ {
+     p=p-1;
+ }
+cout<<"liko svaru ";
+cout<<s<<endl;
+cout<<"liko silingu ";
+cout<<sh<<endl;
+cout<<"liko pencu ";
+cout<<p<<endl;
+    return 0;
+}
